64db526a17c9700cca7ec6fd44e569a7807e7e2a
Abhishek Bansal <abhishek.bansal@sbs-software.com>
2026-06-24 06:08:44 +0530

    initial commit for project structure

---
+++ src/main/java/com/scaleatdesign/springboot_reactive_practice/SpringbootReactivePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.scaleatdesign.springboot_reactive_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactivePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactivePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaleatdesign/springboot_reactive_practice/SpringbootReactivePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaleatdesign.springboot_reactive_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactivePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
